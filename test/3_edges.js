0e5e1bb0b4dc43fca12e280a0b895edc13c9aa96
Camilo Gonzalez Tabares <t-cagon@microsoft.com>
2017-06-07 16:26:53 -0700

    Added tests for POST /v1/cases/:caseId/edges/:edgeId

---
+++ test/3_edges.js
@@ -0,0 +1,48 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = require('chai').Should();
+var api = require('./../api.holograph');
+
+chai.use(chaiHttp);
+
+describe('Edges', () => {
+
+  var edge = {};
+
+  before((done) => {
+    chai.request(api)
+      .get('/v1/cases')
+      .end((req, res) => {
+        edge.caseId = res.body[0]._id;
+        
+        chai.request(api)
+          .get('/v1/cases/' + edge.caseId + '/vertices')
+          .end((req, res) => {
+            edge.source = res.body[0]._id;
+            edge.target = res.body[1]._id;
+
+            done();
+          });
+      });
+  });
+
+  it('should create an edge', (done) => {
+    chai.request(api)
+      .post('/v1/cases/' + edge.caseId + '/edges')
+      .send(edge)
+      .end((req, res) => {
+        res.should.have.status(201);
+
+        res.body.should.have.property('source');
+        res.body.name.should.equal(vertex.source);
+
+        res.body.should.have.property('targe');
+        res.body.type.should.equal(vertex.target);
+
+        res.body.should.have.property('_id');
+        vertex._id = res.body._id;
+
+        done(); 
+      });
+  });
+});
