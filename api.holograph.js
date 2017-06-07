c62866d4ff6272500efb041a2e2543cc886c65f4
Camilo Gonzalez Tabares <t-cagon@microsoft.com>
2017-06-07 08:51:35 -0700

    Removed graph

--- api.holograph.js
+++ api.holograph.js
@@ -2,16 +2,24 @@ var env = require('dotenv').config();
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var express = require('express');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
 
-// Routers
-var graphsRouter = require('./routers/graphs');
 
-app.use('/v1/graphs', graphsRouter);
+
+// Error handler
+app.use((err, req, res, next) => {
+  var response = {
+    message: err.message,
+    status: err.status || 400
+  };
+
+  return res.status(response.status).json(response);
+});
 
 var listener = app.listen(process.env.PORT, () => {
   console.log('Holograph API running in port ' + listener.address().port);
