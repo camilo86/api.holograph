1e8cdd1fd197888376b43786aa89623b41b9ab35
Camilo Gonzalez Tabares <t-cagon@microsoft.com>
2017-06-07 09:11:22 -0700

    Add connecction to db

--- api.holograph.js
+++ api.holograph.js
@@ -1,7 +1,16 @@
 var env = require('dotenv').config();
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
+var mongoose = require('mongoose');
 var express = require('express');
+
+// Connect to db
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB);
+mongoose.connection.on('error', () => {
+  console.log('Could not connect to database');
+  process.exit(1);
+})
 
 var app = express();
 
