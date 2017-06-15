eb7e0ba3aed0670cb3570395bae529568c5c7d82
Camilo Gonzalez Tabares <t-cagon@microsoft.com>
2017-06-15 09:24:20 -0700

    Removed socket.io

--- api.holograph.js
+++ api.holograph.js
@@ -13,10 +13,6 @@ mongoose.connection.on('error', () => {
 })
 
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(3001);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,12 +22,6 @@ app.use(expressValidator());
 var casesRouter = require('./routers/cases');
 
 app.use('/v1/cases', casesRouter);
-
-io.on('connection', function (socket) {
-	socket.emit('case', {
-		message: 'Hello Wolrd'
-	});
-});
 
 // Error handler
 app.use((err, req, res, next) => {
