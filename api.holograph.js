9976f987f7bf0c26584b48a0ea636d82a85368eb
Camilo Gonzalez <t-cagon@microsoft.com>
2017-07-31 10:59:55 -0700

    Connected API to mongodb

--- api.holograph.js
+++ api.holograph.js
@@ -1,6 +1,13 @@
-var restify = require('restify');
+var mongoose = require('mongoose');
 var errs = require('restify-errors');
+var restify = require('restify');
 var server = restify.createServer();
+
+mongoose.connect(process.env.DATABASE || 'mongodb://localhost:27017/holograph', (error) => {
+  if(error) {
+    throw error;
+  }
+});
 
 server.get('/', (req, res, next) => {
   res.send('Hello World');
