644acbcfefd5ca3f30605c6293b913741b2d92c6
Camilo Gonzalez <t-cagon@microsoft.com>
2017-07-31 10:41:38 -0700

    Added basic http server

--- api.holograph.js
+++ api.holograph.js
@@ -1,36 +1,10 @@
-var env = require('dotenv').config();
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var mongoose = require('mongoose');
 var express = require('express');
-
-// Connect to db
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_PROD);
-mongoose.connection.on('error', () => {
-  console.log('Could not connect to database');
-  process.exit(1);
-})
-
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(expressValidator());
-
-// Routers
-var casesRouter = require('./routers/cases');
-
-app.use('/v1/cases', casesRouter);
-
-// Error handler
-app.use((err, req, res, next) => {
-  var response = {
-    message: err.message,
-    status: err.status || 400
-  };
-
-  return res.status(response.status).json(response);
+app.get('/', (req, res, next) => {
+  return res.json({
+    message: 'Hello World!'
+  });
 });
 
 var listener = app.listen(process.env.PORT || 3000, () => {
--- models/edges.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var edgesSchema = new mongoose.Schema({
-  source: { type: mongoose.Schema.Types.ObjectId, ref: 'Vertex', required: true },
-  target: { type: mongoose.Schema.Types.ObjectId, ref: 'Vertex', required: true },
-  case: { type: mongoose.Schema.Types.ObjectId, ref: 'Case', required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Edge', edgesSchema);
